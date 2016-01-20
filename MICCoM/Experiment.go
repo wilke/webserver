30f3d99e56d07d960a5ff35e084124b243cc451a
Andreas Wilke <wilke@mcs.anl.gov>
2016-01-20 11:22:05 -0600

    Experiment class

--- MICCoM/Experiment.go
+++ MICCoM/Experiment.go
@@ -4,24 +4,35 @@ import (
 	//"fmt"
 	"errors"
 	"fmt"
-	"github.com/wilke/webserver/Frame"
+	"github.com/wilke/webserver/CollectionJson"
+	//"log"
+	//"net/http"
 	"time"
 )
 
 type Data struct {
-	Name     string
-	ID       string
-	Version  string
-	Date     string
-	Duration string
-	Files    []string
-	Samples  []string
+	Name     string   `bson:"name"`
+	ID       string   `bson:"ID"`
+	Version  string   `bson:"version"`
+	Date     string   `bson:"date"`
+	Duration string   `bson:"duration"`
+	Files    []string `bson:"files"`
+	Samples  []string `bson:"samples"`
+	Analysis interface{}
+	Workflow interface{}
 }
 
 type Experiment struct {
-	Frame.Item
+	CollectionJson.Item
 	Data Data `json:"data"`
 }
+
+//var template CollectionJson.Template{}
+
+var experimentTemplate = CollectionJson.Template{
+	{Name: "Name", Value: "String", Prompt: "Experiment name"},
+	{Name: "Date", Value: "yyyy-mm-dd", Prompt: "Start date of experiment"},
+	{Name: "Duration", Value: "integer", Prompt: "Duration of the experiment in seconds"}}
 
 func NewExperiment(id string) (Experiment, error) {
 
@@ -43,6 +54,11 @@ func NewExperiment(id string) (Experiment, error) {
 	return e, nil
 }
 
+func (e Experiment) GetTemplate() (CollectionJson.Template, error) {
+	template := experimentTemplate
+	return template, nil
+}
+
 func (e Experiment) GetItem() Experiment {
 
 	var err error
@@ -56,20 +72,33 @@ func (e Experiment) GetItem() Experiment {
 	return e
 }
 
-func (e Experiment) AddData(c interface{}) {
+func (e Experiment) AddToData(c interface{}) {
 	//var alist []ExperimentStruct
-	t := c.(Frame.Collection)
+	t := c.(CollectionJson.Collection)
 	alist := t.Items.([]Experiment)
 	t.Items = append(alist, e)
 
 }
 
 // Add experiment to items list in collection
-func (e Experiment) AddItem(c *Frame.Collection) int {
+func (e Experiment) AddToItems(c *CollectionJson.Collection) int {
 	//var alist []ExperimentStruct
 
-	alist := c.Items.([]Experiment)
-	c.Items = append(alist, e)
+	if c.Items == nil {
+		c.Items = []Experiment{e}
+	} else {
+		alist := c.Items.([]Experiment)
+		c.Items = append(alist, e)
+	}
 
 	return len(c.Items.([]Experiment))
+}
+
+func (e Experiment) ToData() (&[]CollectionJson.DataItem , error) {
+	
+	var dl  []CollectionJson.DataItem
+	var err error
+	
+	
+	return dl , err
 }
